93076c286bf0cbc459f10121cca2d1c3f3bd0e52
Arpad Magosanyi <mag@magwas.rulez.org>
2011-05-06 12:33:39 +0200

    preference patch from Phil, cosmetics on structured report
    Thank you, Phil!

---
+++ src/org/rulez/magwas/styledhtml/StyledHtmlPlugin.java
@@ -0,0 +1,27 @@
+package org.rulez.magwas.styledhtml;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class StyledHtmlPlugin extends AbstractUIPlugin {
+
+    /**
+     * ID of the plug-in
+     */
+    public static final String PLUGIN_ID = "org.rulez.magwas.styledhtml";
+
+    /**
+     * The shared instance
+     */
+    public static StyledHtmlPlugin INSTANCE;
+
+    /**
+     * The constructor.
+     */
+    public StyledHtmlPlugin() {
+        INSTANCE = this;
+    }
+
+}
