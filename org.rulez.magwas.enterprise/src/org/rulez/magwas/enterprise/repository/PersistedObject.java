c19a4d8635d798cf0f3f7be2dcbb8624051c4c67
Arpad Magosanyi <mag@magwas.rulez.org>
2011-11-12 15:07:39 +0100

    The checkin maybe works correctly

--- org.rulez.magwas.enterprise/src/org/rulez/magwas/enterprise/repository/PersistedObject.java
+++ org.rulez.magwas.enterprise/src/org/rulez/magwas/enterprise/repository/PersistedObject.java
@@ -41,12 +41,12 @@ public class PersistedObject {
 	private String name=null;
 	private String documentation=null, linetext = null;
 	private String type=null;
-	private Integer subtype=null;
+	private Integer subtype=null, alignment=null, textposition=null, linewidth=null;
 	private String source=null,target=null, element=null; //these are references to other objects
 	private String font=null, fontcolor=null,fillcolor=null; //graphical properties
 	private Integer x1=null, x2=null, y1=null, y2=null;
 	private String key=null, value=null;
-	private float weight;
+	private Float weight;
 
 	private Connection con;
 	private Integer versionid;
@@ -83,23 +83,30 @@ public class PersistedObject {
 		return new PersistedObject(con, versionid, node,parent);
 	}
 	
-	private void setInt(PreparedStatement stmt, int index, Integer value) throws SQLException {
+	private void setSQLInt(PreparedStatement stmt, int index, Integer value) throws SQLException {
 		if(null == value) {
 			stmt.setNull(index, Types.INTEGER);
 		} else {
 			stmt.setInt(index, value);
 		}
 	}
+	private void setSQLFloat(PreparedStatement stmt, int index, Float value) throws SQLException {
+		if(null == value) {
+			stmt.setNull(index, Types.FLOAT);
+		} else {
+			stmt.setFloat(index, value);
+		}
+	}
 
 	public void persist() throws SQLException {
 		if (persisted) {
-			System.out.println("already persisted "+this);
+			//System.out.println("already persisted "+this);
 			return;
 		}
 		if(this != parent) {
 			parent.persist();
 		}
-		System.out.println("persisting "+this);
+		//System.out.println("persisting "+this);
 		persistSQL();
 		persisted = true;
 		for(PersistedObject prop : properties) {
@@ -113,10 +120,17 @@ public class PersistedObject {
 	
 	private void persistSQL() throws SQLException {
 		if (null != id) {
+			assert(key==null);
+			assert(value==null);
+			assert(x1==null);
+			assert(y1==null);
+			assert(x2==null);
+			assert(y2==null);
+			assert(weight==null);
 			PreparedStatement psInsertObject = con.prepareStatement(
 					"insert into object_view " +
-					"(version, id, parent, name, documentation, type, source, target, element, font, fontcolor, subtype, fillcolor, linetext)" +
-					"values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+					"(version, id, parent, name, documentation, type, source, target, element, font, fontcolor, subtype, fillcolor, linetext, alignment, textposition, linewidth)" +
+					" values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 			psInsertObject.setInt(1, versionid);
 			psInsertObject.setString(2, id);
 			psInsertObject.setString(3, parent.getId());
@@ -128,13 +142,29 @@ public class PersistedObject {
 			psInsertObject.setString(9, element);
 			psInsertObject.setString(10, font);
 			psInsertObject.setString(11, fontcolor);
-			setInt(psInsertObject,12,subtype);
+			setSQLInt(psInsertObject,12,subtype);
 			psInsertObject.setString(13, fillcolor);
 			psInsertObject.setString(14, linetext);
-			System.out.println("executing "+psInsertObject);
+			setSQLInt(psInsertObject,15, alignment);
+			setSQLInt(psInsertObject,16, textposition);
+			setSQLInt(psInsertObject,17, linewidth);
+			//System.out.println("executing "+psInsertObject+";");
 			psInsertObject.execute();
 			psInsertObject.close();
 		} else {
+			assert(name==null);
+			assert(documentation==null);
+			assert(source==null);
+			assert(target==null);
+			assert(element==null);
+			assert(font==null);
+			assert(fontcolor==null);
+			assert(subtype==null);
+			assert(fillcolor==null);
+			assert(linetext==null);
+			assert(alignment==null);
+			assert(textposition==null);
+			assert(linewidth==null);
 			//property
 			PreparedStatement psInsertObject = con.prepareStatement(
 					"insert into property_view " +
@@ -145,19 +175,19 @@ public class PersistedObject {
 			psInsertObject.setString(3, type);
 			psInsertObject.setString(4, key);
 			psInsertObject.setString(5, value);
-			setInt(psInsertObject,6,x1);
-			setInt(psInsertObject,7,y1);
-			setInt(psInsertObject,8,x2);
-			setInt(psInsertObject,9,y2);
-			psInsertObject.setFloat(10,weight);
-			System.out.println("executing "+psInsertObject);
+			setSQLInt(psInsertObject,6,x1);
+			setSQLInt(psInsertObject,7,y1);
+			setSQLInt(psInsertObject,8,x2);
+			setSQLInt(psInsertObject,9,y2);
+			setSQLFloat(psInsertObject,10,weight);
+			//System.out.println("executing "+psInsertObject+";");
 			psInsertObject.execute();
 			psInsertObject.close();
 		}
 	}
 
 	public void ready() {
-		System.out.println("readying"+this);
+		//System.out.println("readying"+this);
 		if (null == id){
 			assert(parent != this);
 			parent.addToProperties(this);
@@ -219,7 +249,7 @@ public class PersistedObject {
 		}
 
 		if(node instanceof IDiagramModel) {
-			//FIXME clash on subtype setSubtype(((IDiagramModel) node).getConnectionRouterType());
+			setAlignment(((IDiagramModel) node).getConnectionRouterType());
 		}
 
 		if(node instanceof IDiagramModelArchimateConnection) {
@@ -242,11 +272,13 @@ public class PersistedObject {
 		if(node instanceof IFontAttribute) {
 			setFont(((IFontAttribute) node).getFont());
 			setFontcolor(((IFontAttribute) node).getFontColor());
-			// FIXME clash on subtype setSubtype(((IFontAttribute) node).getTextAlignment());			
+			setAlignment(((IFontAttribute) node).getTextAlignment());			
 		}
 		
 		if(node instanceof IDiagramModelObject) {
-			//FIXME IBounds bounds = ((IDiagramModelObject) node).getBounds();
+			IBounds bounds = ((IDiagramModelObject) node).getBounds();
+			//System.out.println(bounds.eClass().getName());
+			addNode(bounds,this);
 			EList<IDiagramModelConnection> scs = ((IDiagramModelObject) node).getSourceConnections();
 			for (IDiagramModelConnection sc: scs) {
 				addNode( sc, this);
@@ -275,15 +307,15 @@ public class PersistedObject {
 		 */
 		if(node instanceof IDiagramModelConnection) {
 			setLinetext(((IDiagramModelConnection) node).getText());
-			//FIXME int textpos = ((IDiagramModelConnection) node).getTextPosition();
+			setTextposition(((IDiagramModelConnection) node).getTextPosition());
 			setSource(((IDiagramModelConnection) node).getSource().getId());
 			setTarget(((IDiagramModelConnection) node).getTarget().getId());
 			EList<IDiagramModelBendpoint> bendpoints = ((IDiagramModelConnection) node).getBendpoints();
 			for (IDiagramModelBendpoint bp: bendpoints) {
 				addNode( bp, this);
 			}
-			// FIXME int linewith = ((IDiagramModelConnection) node).getLineWidth();
-			// FIXME String linecolor = ((IDiagramModelConnection) node).getLineColor()
+			setLinewidth(((IDiagramModelConnection) node).getLineWidth());
+			setFillcolor(((IDiagramModelConnection) node).getLineColor());
 			String t = ((IDiagramModelConnection) node).getType();
 			assert(t == null);
 			//setType(t);
@@ -313,9 +345,7 @@ public class PersistedObject {
 			for (EObject kid : fl) {
 				addNode( kid, this);
 			}
-			System.out.println("foldertype name="+ ((IFolder) node).getType().getName());
-			System.out.println("foldertype literal="+ ((IFolder) node).getType().getLiteral());
-			id = ((IFolder) node).getType().getName(); //This is intentional, to make merging models easier. FIXME make sure not getLiteral is the right method
+			id = ((IFolder) node).getType().getName(); //This is intentional, to make merging models easier.
 		}
 		ready();
 	}
@@ -349,7 +379,7 @@ public class PersistedObject {
 		return type;
 	}
 	public void setType(String type) {
-		System.out.println("type="+type);
+		//System.out.println("type="+type);
 		q("this.type");
 		this.type = type;
 	}
@@ -448,18 +478,33 @@ public class PersistedObject {
 		q("this.weight");
 		this.weight = value;
 	}
+	
+	public void setAlignment(Integer alignment) {
+		q("this.alignment");
+		this.alignment = alignment;
+	}
+	
+	public void setTextposition(Integer alignment) {
+		q("this.textposition");
+		this.textposition = alignment;
+	}
+	
+	public void setLinewidth(Integer alignment) {
+		q("this.linewidth");
+		this.linewidth = alignment;
+	}
 	/*
 	 * trace the usage of overloaded variables. This is a debug tool
 	 */
 
 	private void q(String name) {
+		/*
 		Throwable t = new Throwable();
 		StackTraceElement[] elements = t.getStackTrace();
-
 		int callerline = elements[2].getLineNumber();
 		System.out.println(name+":"+callerline);
+		*/
 
-		System.out.println(); 
 		if (ovrvars.contains(name)) {
 			throw new Error("multiple override for "+ name+":"+ovrvars);
 		}
